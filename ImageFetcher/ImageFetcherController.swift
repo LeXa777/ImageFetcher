5b5ee6006f385cf8ef26724a99cbef20e8504abc
Alex <akrim777@gmail.com>
2018-02-09 19:01:22 +0000

    initial

---
+++ ImageFetcher/ImageFetcherController.swift
@@ -0,0 +1,19 @@
+//
+//  ImageFetcherController.swift
+//  ImageFetcher
+//
+//  Created by Alex Akrimpai on 09/02/2018.
+//  Copyright © 2018 CodingWarrior. All rights reserved.
+//
+
+import UIKit
+
+class ImageFetcherController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .green
+    }
+}
+
